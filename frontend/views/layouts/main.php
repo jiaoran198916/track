b1cdbf503b0ad15bc95488af0acbcc954e54b18e
jiaoran <920495391@qq.com>
2019-07-28 11:54:12 +0800

    头部修改

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -20,30 +20,81 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
-    <title><?= '电影原声网' ?></title>
+    <title>电影原声网</title>
     <?php $this->head() ?>
 </head>
 <body>
 <?php $this->beginBody() ?>
-<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+<nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
+        <!-- Brand and toggle get grouped for better mobile display -->
         <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="" href="/" > <img alt="Brand" style="height: 55px;" src="/static/images/logo.png"> </a>
+            <a class="navbar-brand" href="/"><img alt="Brand" src="/static/images/logo1.png"></a>
         </div>
-        <div class="collapse navbar-collapse" id="navbar">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a data-toggle="modal" data-target="#searchModal">搜索</a></li>
-                <li><a href="/about">关于</a></li>
+
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+                <li class="<?= (Yii::$app->request->getUrl() == '/' || Yii::$app->request->getUrl() == '') ? 'active':''?>"><a href="/">首页 <span class="sr-only">(current)</span></a></li>
+                <li class="<?= (Yii::$app->request->getUrl() == '/movie') ? 'active':''?>"><a href="/movie">电影</a></li>
+                <li class="<?= (Yii::$app->request->getUrl() == '/about') ? 'active':''?>"><a href="/about">关于</a></li>
             </ul>
-        </div>
-    </div>
+            <form class="navbar-form navbar-left" action="<?= Url::to(['movie/index'])?>" method="get">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="电影名、音乐人" name="MovieSearch[name]">
+                    <span class="input-group-btn">
+                        <button type="submit" class="btn btn-default btn-sm"><span class="glyphicon glyphicon-search"></span></button>
+                    </span>
+<!--                    <button type="submit" class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>-->
+<!--                    <span class="input-group-btn">-->
+<!--                            <button class="btn btn-info" type="button"><span class="glyphicon glyphicon-search"></span></button>-->
+<!--                    </span>-->
+                </div>
+
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="javascript:void(0)">游客</a></li>
+<!--                <li class="dropdown">-->
+<!--                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>-->
+<!--                    <ul class="dropdown-menu">-->
+<!--                        <li><a href="#">Action</a></li>-->
+<!--                        <li><a href="#">Another action</a></li>-->
+<!--                        <li><a href="#">Something else here</a></li>-->
+<!--                        <li role="separator" class="divider"></li>-->
+<!--                        <li><a href="#">Separated link</a></li>-->
+<!--                    </ul>-->
+<!--                </li>-->
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
 </nav>
+<!--<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">-->
+<!--    <div class="container">-->
+<!--        <div class="navbar-header">-->
+<!--            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">-->
+<!--                <span class="sr-only">Toggle navigation</span>-->
+<!--                <span class="icon-bar"></span>-->
+<!--                <span class="icon-bar"></span>-->
+<!--                <span class="icon-bar"></span>-->
+<!--            </button>-->
+<!--            <a class="" href="/" > <img alt="Brand" style="height: 55px;" src="/static/images/logo.png"> </a>-->
+<!--        </div>-->
+<!--        <div class="collapse navbar-collapse" id="navbar">-->
+<!--            <ul class="nav navbar-nav navbar-right">-->
+<!--                <li><a data-toggle="modal" data-target="#searchModal">搜索</a></li>-->
+<!--                <li><a href="/about">关于</a></li>-->
+<!--            </ul>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--</nav>-->
+<?= Yii::$app->request->getUrl()?>
 <?= $content ?>
 
 <!--<div class="footer" id="footer">-->
