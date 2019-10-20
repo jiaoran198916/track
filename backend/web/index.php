13b3eb65d3674a177d13f4f7ca096945310ba46c
huxiaowei <huxiaowei@otbmall.com>
2019-10-20 16:36:39 +0800

    添加引入

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,11 +1,14 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_ENV') or define('YII_ENV', 'prod');
 
 require(__DIR__ . '/../../vendor/autoload.php');
 require(__DIR__ . '/../../vendor/yiisoft/yii2/Yii.php');
 require(__DIR__ . '/../../common/config/bootstrap.php');
 require(__DIR__ . '/../config/bootstrap.php');
+
+require(__DIR__ . '/../../vendor/qiniu/php-sdk/autoload.php');
+
 
 
 $config = yii\helpers\ArrayHelper::merge(
