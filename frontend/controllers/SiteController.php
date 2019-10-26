ac107cc5d036100b0bbe17b20fc18c85f824ff2c
jiaoran <920495391@qq.com>
2019-10-26 09:00:20 +0800

    修改邮箱

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -164,7 +164,7 @@ class SiteController extends Controller
         $model = new ContactForm();
         if ($model->load(Yii::$app->request->post()) && $model->validate()) {
             if ($model->sendEmail(Yii::$app->params['adminEmail'])) {
-                Yii::$app->session->setFlash('success', 'Thank you for contacting us. We will respond to you as soon as possible.');
+                Yii::$app->session->setFlash('success', 'Thanks you for contacting us. We will respond to you as soon as possible.');
             } else {
                 Yii::$app->session->setFlash('error', 'There was an error sending email.');
             }
