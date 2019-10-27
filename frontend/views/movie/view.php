fc806c829e94d5f5929da46cf01475c66d9c5de3
jiaoran <920495391@qq.com>
2019-10-27 12:31:44 +0800

    演奏者信息

--- frontend/views/movie/view.php
+++ frontend/views/movie/view.php
@@ -147,30 +147,6 @@ use yii\helpers\Url;
                                                 <?php
                                                 endif;?>
                                             </div>
-                                            <!--<div class="title-hd-sm">
-                                                <h4>User reviews</h4>
-                                                <a href="#" class="time">See All 56 Reviews <i class="ion-ios-arrow-right"></i></a>
-                                            </div>-->
-                                            <!-- movie user review -->
-                                            <!--<div class="mv-user-review-item">
-                                                <h3>Best Marvel movie in my opinion</h3>
-                                                <div class="no-star">
-                                                    <i class="ion-android-star"></i>
-                                                    <i class="ion-android-star"></i>
-                                                    <i class="ion-android-star"></i>
-                                                    <i class="ion-android-star"></i>
-                                                    <i class="ion-android-star"></i>
-                                                    <i class="ion-android-star"></i>
-                                                    <i class="ion-android-star"></i>
-                                                    <i class="ion-android-star"></i>
-                                                    <i class="ion-android-star"></i>
-                                                    <i class="ion-android-star last"></i>
-                                                </div>
-                                                <p class="time">
-                                                    17 December 2016 by <a href="#"> hawaiipierson</a>
-                                                </p>
-                                                <p>This is by far one of my favorite movies from the MCU. The introduction of new Characters both good and bad also makes the movie more exciting. giving the characters more of a back story can also help audiences relate more to different characters better, and it connects a bond between the audience and actors or characters. Having seen the movie three times does not bother me here as it is as thrilling and exciting every time I am watching it. In other words, the movie is by far better than previous movies (and I do love everything Marvel), the plotting is splendid (they really do out do themselves in each film, there are no problems watching it more than once.</p>
-                                            </div>-->
                                         </div>
                                         <div class="col-md-4 col-xs-12 col-sm-12">
                                             <div class="sb-it">
@@ -252,7 +228,7 @@ use yii\helpers\Url;
                                                             <span style="color: #abb7c4;margin-right: 20px;"><?= $v->min.":".$v->sec ?></span>
                                                             <a><?= $v->name ?>&nbsp;<?= $v->ename ?></a>
                                                         </div>
-                                                        <p><?= $v->musicians ?>Taylor Swift</p>
+                                                        <p><?= $v->musicians ?></p>
                                                     </div>
 
                                                 <?php
