04db6ad842c5cb6a3ff883e7155131f2b354cda6
jiaoran <920495391@qq.com>
2019-11-15 09:38:57 +0800

    错误提示

--- backend/views/site/error.php
+++ backend/views/site/error.php
@@ -21,7 +21,7 @@ $this->title = $name;
         The above error occurred while the Web server was processing your request.
     </p>
     <p>
-        Please contact us if you think this is a server error. Thank you.
+        Please contact us if you think this is a server error.
     </p>
 
 </div>
